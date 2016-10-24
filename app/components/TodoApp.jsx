f70954f65fb9aa2e0141636545ff27cd725b8d4a
Piotr Roczniak <proczniak@gmail.com>
2016-10-24 16:35:31 +0200

    created TodoList component and Todo component, wired up props to make it work

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,25 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return ({
+      todos: [
+        {id: 1, text: "walk the dog"},
+        {id: 2, text: "feed the cat"},
+        {id: 3, text: "clean the yard"},
+        {id: 4, text: "slap the bitch"}
+      ]
+    });
+  },
 
   render: function() {
-    return <div>Todo App.jsx</div>
+    var {todos}=this.state;
+    return (
+      <div>
+        <TodoList todos={todos}/>
+      </div>
+    );
   }
 
 });
