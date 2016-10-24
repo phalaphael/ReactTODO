47833049c7e08c81dc833c59d0be195c09c7aac8
Piotr Roczniak <proczniak@gmail.com>
2016-10-24 17:19:33 +0200

    add test suit for list components

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,4 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
+var $ = require('jQuery');
+
 var Todo = require('Todo');
 
 describe('Todo', () => {
