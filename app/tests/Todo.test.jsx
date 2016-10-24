e8bc4da77b196918ceb4365beca8f166f7375ef5
Piotr Roczniak <proczniak@gmail.com>
2016-10-24 16:43:56 +0200

    Add basic tests for Todo, TodoList, TodoApp components

---
+++ app/tests/Todo.test.jsx
@@ -0,0 +1,8 @@
+var expect = require('expect');
+var Todo = require('Todo');
+
+describe('Todo', () => {
+  it('should exist', () => {
+    expect(Todo).toExist();
+  });
+});
---
+++ app/tests/TodoList.test.jsx
@@ -0,0 +1,8 @@
+var expect = require('expect');
+var TodoList = require('TodoList');
+
+describe('TodoList', () => {
+  it('should exist', () => {
+    expect(TodoList).toExist();
+  });
+});
