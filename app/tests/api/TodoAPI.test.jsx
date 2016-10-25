54c05d456a987792bd17b825ec5b6145713b9428
Piotr Roczniak <proczniak@gmail.com>
2016-10-25 18:10:39 +0200

    add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -51,5 +51,49 @@ describe('TodoAPI', ()=>{
 
     });
 
+    describe('filterTodos', ()=> {
+      var todos = [
+        {
+          id: 1,
+          text: 'Some text here',
+          completed: true
+        }, {
+            id: 2,
+            text: 'other text here',
+            completed: false
+        }, {
+            id: 3,
+            text: 'Some text here',
+            completed: true
+        }
+      ];
+
+      it('should return all items if showCompleted is true', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+
+      it('should return only not completed items if showCompleted is false', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+        expect(filteredTodos.length).toBe(1);
+      });
+
+      it('should sort non-completed items in front', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+      });
+
+      it('should filter todos by searchText', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+        expect(filteredTodos.length).toBe(2);
+      });
+
+      it('should return all todos searchText is empty', () => {
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos.length).toBe(3);
+      });
+
+    });
+
   });
 });
