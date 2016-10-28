844a4e85b2d84e89dbe8bd7ceeb826b13355a2c9
Piotr Roczniak <proczniak@gmail.com>
2016-10-28 20:14:42 +0200

    Refactor app to work with localstorage

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -53,6 +53,24 @@ describe('Reducers', () => {
       var res = reducers.todosReducer(df(todos), df(action));
       expect(res[0].completed).toEqual(false);
       expect(res[0].completedAt).toEqual(undefined);
+    });
+
+    it('should add existing todos', ()=>{
+      var todos = [{
+        id: '111',
+        text: 'anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 33000
+      }];
+      var action = {
+        type: 'ADD_TODOS',
+        todos
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0]).toEqual(todos[0]);
 
     });
   });
